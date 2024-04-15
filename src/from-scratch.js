e7b7717502c1bdad894236e13d6c81b2b3176b75
toeten <n.santiago0228@gmail.com>
2024-04-15 13:20:02 -0400

    answered question 3

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -24,13 +24,27 @@ const myMap = (arr, callback) => {
 const myNums = [1, 4, 9, 16];
 const myDoubledNums = myMap(myNums, (x) => x * 2);
 
-console.log(myDoubledNums);
+//console.log(myDoubledNums);
 // expected output: [2, 8, 18, 32]
-console.log(myNums);
+//console.log(myNums);
 // expected output: [1, 4, 9, 16] unaffected!
 
-const myFind = () => {
+const myFind = (arr, callback) => {
+  let copy = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      return arr[i]
+    }
+  }
+  return;
 };
+
+const myNames2 = ['Alice', 'Bob', 'Charlie', 'Debbie', 'Barry'];
+const nameHasB = myFind(myNames2, (name) => name.includes('B'));
+//console.log(nameHasB); // 'Bob' not 'Barry' because 'Bob' is first
+
+const nameHasZ = myFind(myNames, (name) => name.includes('Z'));
+//console.log(nameHasZ); // undefined
 
 const myFilter = () => {
 };
