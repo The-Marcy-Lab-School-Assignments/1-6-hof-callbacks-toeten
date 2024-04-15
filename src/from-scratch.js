84eecb78d050ac18cec2bec609631ffbd6b4aa43
toeten <n.santiago0228@gmail.com>
2024-04-15 12:12:22 -0400

    answered question 1

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,6 +1,18 @@
-const myForEach = () => {
-};
+const myForEach = (arr, callback) => {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i])
+  }
+}
+const myNames = ['Alice', 'Bob', 'Charlie', 'Debbie'];
+const result = myForEach(myNames, (name) => console.log(`Hi, ${name}!`));
+/* That will log */
+// Hi, Alice!
+// Hi, Bob!
+// Hi, Charlie!
+// Hi, Debbie!
 
+console.log(result); // undefined
+console.log(myNames); // ['Alice', 'Bob', 'Charlie', 'Debbie']
 const myMap = () => {
 };
 
