6d4e91910109be4da175ee0d19f827cd4700f315
toeten <n.santiago0228@gmail.com>
2024-04-15 14:26:44 -0400

    answered question 11

--- src/debug.js
+++ src/debug.js
@@ -18,8 +18,15 @@ the rest of the values in the array */
 
 
 const sortUsersBy = (users, sortingFunction) => {
-  return [...users].sort(sortingFunction());
+  return [...users].sort(sortingFunction)
 };
+//just remove the (); 
+/*remember: functions are first class citizens
+which means they can be passed as arguments to other functions 
+just like any other value, such as strings or numbers. 
+when you pass a function as an argument to another function, 
+you're passing a reference to that function. */
+
 
 const logEachName = (names) => {
   return names.forEach(console.log());
