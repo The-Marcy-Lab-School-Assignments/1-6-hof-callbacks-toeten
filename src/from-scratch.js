f851b34dab76d0b2d7833caddbf99e05c76c13a2
toeten <n.santiago0228@gmail.com>
2024-04-15 12:22:50 -0400

    answered question 2

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -4,17 +4,30 @@ const myForEach = (arr, callback) => {
   }
 }
 const myNames = ['Alice', 'Bob', 'Charlie', 'Debbie'];
-const result = myForEach(myNames, (name) => console.log(`Hi, ${name}!`));
+//const result = myForEach(myNames, (name) => console.log(`Hi, ${name}!`));
 /* That will log */
 // Hi, Alice!
 // Hi, Bob!
 // Hi, Charlie!
 // Hi, Debbie!
 
-console.log(result); // undefined
-console.log(myNames); // ['Alice', 'Bob', 'Charlie', 'Debbie']
-const myMap = () => {
-};
+//console.log(result); // undefined
+//console.log(myNames); // ['Alice', 'Bob', 'Charlie', 'Debbie']
+
+const myMap = (arr, callback) => {
+  let copy = [...arr]
+  for (let i = 0; i < arr.length; i++) {
+    copy[i] = callback(copy[i]);
+  }
+  return copy
+}
+const myNums = [1, 4, 9, 16];
+const myDoubledNums = myMap(myNums, (x) => x * 2);
+
+console.log(myDoubledNums);
+// expected output: [2, 8, 18, 32]
+console.log(myNums);
+// expected output: [1, 4, 9, 16] unaffected!
 
 const myFind = () => {
 };
